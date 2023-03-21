526d93e56bd70c4c5f9dddbbae0ead05ef540aba
twachowicz <twachowicz@sii.pl>
2023-03-21 21:43:13 +0100

    Windows and Tabs - finished

--- src/test/java/com/sii/selenium/basic/WindowsAndTabsTests.java
+++ src/test/java/com/sii/selenium/basic/WindowsAndTabsTests.java
@@ -2,14 +2,21 @@ package com.sii.selenium.basic;
 
 import com.sii.selenium.BaseTest;
 import com.sii.selenium.constants.TestTagConstants;
+import com.sii.selenium.utils.TablesUtils;
 import com.sii.selenium.utils.WebPageUtils;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import static com.sii.selenium.utils.WebPageUtils.*;
+
+import java.util.List;
 
 public class WindowsAndTabsTests extends BaseTest {
 
     private final String windowsWebPageUrl = baseUrl + webAddresses.get("widowsAndTabs");
+    private final int REQUIRED_MOUNTAIN_HEIGHT = 4000;
 
     @Test
     @Tag(TestTagConstants.WINDOWS_AND_TABS)
@@ -21,34 +28,30 @@ public class WindowsAndTabsTests extends BaseTest {
         String baseWindow = driver.getWindowHandle();
         driver.findElement(By.cssSelector("#newBrowserWindow")).click();
 
-        switchWindowOrTab(baseWindow);
+        switchWindowOrTab(baseWindow, driver);
 
-        driver.findElement(By.cssSelector(".table"));
-        //•Execute steps from Tables exercise on the newly opened window
+        List<WebElement> rowsWithSwitzerland = driver.findElements(
+                By.xpath("//tr/td[contains(.,'Switzerland')]/.."));
+        TablesUtils.printRowsWithHeightMoreThan(rowsWithSwitzerland, REQUIRED_MOUNTAIN_HEIGHT);
 
-        //•Close window
+        closeWindowAndGetBackToBase(baseWindow, driver);
 
-        // •Click 'New Message Window' button
+        driver.findElement(By.xpath("//button[@id='newMessageWindow']")).click();
 
-        //•Switch to a new window
+        switchWindowOrTab(baseWindow, driver);
 
-        //•7.Print out the text that is written in a new window
+        System.out.println(driver.findElement(By.xpath("//body")).getText());
 
-        //•Close window
-        // •Click on 'New Browser Tab' button
+        closeWindowAndGetBackToBase(baseWindow, driver);
 
-        //•Switch to a new tab
-        //•Execute steps from Tables exercise on the new opened window
-        //•Close tab
+        driver.findElement(By.xpath("//button[@id='newBrowserTab']")).click();
+
+        switchWindowOrTab(baseWindow, driver);
+
+        rowsWithSwitzerland = driver.findElements(
+                By.xpath("//tr/td[contains(.,'Switzerland')]/.."));
+        TablesUtils.printRowsWithHeightMoreThan(rowsWithSwitzerland, REQUIRED_MOUNTAIN_HEIGHT);
+
+        closeWindowAndGetBackToBase(baseWindow, driver);
     }
-
-    private void switchWindowOrTab(String baseWindow) {
-        for (String windowHandle : driver.getWindowHandles()) {
-            if (!baseWindow.contentEquals(windowHandle)) {
-                driver.switchTo().window(windowHandle);
-                break;
-            }
-        }
-    }
-
 }
--- src/test/java/com/sii/selenium/utils/WebPageUtils.java
+++ src/test/java/com/sii/selenium/utils/WebPageUtils.java
@@ -7,4 +7,19 @@ import java.util.function.Consumer;
 public class WebPageUtils {
 
     public static Consumer<WebDriver> maximiseWindow = d -> d.manage().window().maximize();
+
+    public static void switchWindowOrTab(String baseWindow, WebDriver driver) {
+        for (String windowHandle : driver.getWindowHandles()) {
+            if (!baseWindow.contentEquals(windowHandle)) {
+                driver.switchTo().window(windowHandle);
+                break;
+            }
+        }
+    }
+
+    public static void closeWindowAndGetBackToBase(String baseWindow, WebDriver driver) {
+        driver.close();
+        driver.switchTo().window(baseWindow);
+    }
+
 }
